dee73fcea0fbe9c82cceb347a48abe326a7bd9cd
bachil123 <57742208+bachil123@users.noreply.github.com>
2019-11-14 11:44:41 +0530

    test from pycharm

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello world from GIT! -- Bhasakr"
+    return "hello world from GIT! -- Bhasakr - PYCHARM FROM"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0") 
