4220d2fdf5a60a1b31992feb75892aea1236aab0
bachil123 <57742208+bachil123@users.noreply.github.com>
2019-11-14 11:06:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "hello world from GIT!"
+    return "hello world from GIT! -- Bhasakr"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0") 
